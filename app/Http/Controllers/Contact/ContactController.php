459a9f85c5877c752ad1fef84ddee47854aeec3a
Shahoriar007 <shahoriar.fahim@gmail.com>
2024-04-28 13:04:51 +0600

    test

--- app/Http/Controllers/Contact/ContactController.php
+++ app/Http/Controllers/Contact/ContactController.php
@@ -24,9 +24,18 @@ class ContactController extends Controller
                 'message' => 'nullable|string',
             ]);
 
+            info($request->all());
+
             $ip = $request->ip();
+
+            info($ip);
+
             $response = Http::get("http://ip-api.com/json/$ip?fields=country");
+
+            info($response->json());
             $country = $response->json()['country'] ?? null;
+
+            info($country);
 
             $contact = new Contact();
             $contact->name = $request->name;
